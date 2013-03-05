4b461944bc3a8dfc75e7d6cf1092a909e8a52099
paolobittante <paolo.bittante@gmail.com>
2013-03-05 18:19:05 +0100

    Update DeadCell.java

--- DeadCell.java
+++ DeadCell.java
@@ -3,14 +3,14 @@ public class DeadCell extends Cell{
 	private int numberOfN;
 	private static final long serialVersionUID = 1L;
 	protected DeadCell(int x, int y) {
-		super(x, y, "deadCell.gif");		
+		super(x, y, "deadCell.gif");
 	}
-	@Override 
-	public int getNumbOfN(){
+	
+	public synchronized int getNumbOfN(){
 		return numberOfN;
 	}
-	@Override
-	public void incrementNumbOfN(){
+	
+	public synchronized void incrementNumbOfN(){
 		numberOfN++;
 	}
 }

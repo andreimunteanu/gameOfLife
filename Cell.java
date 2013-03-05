e8d5cbc5bf5cbd5ca3c7496e2319679e5c078d15
paolobittante <paolo.bittante@gmail.com>
2013-03-05 18:18:38 +0100

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -1,13 +1,11 @@
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
-public abstract class Cell extends JButton{
+public class Cell extends JButton{
 	private static final long serialVersionUID = 1L;
 	public static final int CELL_SIZE = 10;
 	protected Cell(int x, int y, String fileName){
 		super(new ImageIcon(fileName));
 		setBounds(x * Cell.CELL_SIZE, y * Cell.CELL_SIZE, Cell.CELL_SIZE, Cell.CELL_SIZE);
 	}
-	public abstract void incrementNumbOfN();
-	public abstract int getNumbOfN();
 }

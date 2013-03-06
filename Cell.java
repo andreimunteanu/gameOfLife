ced7c1a299ecfad8d8ddc2142a9217777c85c6e6
niccolomarastoni <eightnine@gmail.com>
2013-03-06 10:54:26 +0100

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -2,10 +2,22 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class Cell extends JButton{
+	private int x;
+	private int y;
 	private static final long serialVersionUID = 1L;
 	public static final int CELL_SIZE = 10;
 	protected Cell(int x, int y, String fileName){
 		super(new ImageIcon(fileName));
 		setBounds(x * Cell.CELL_SIZE, y * Cell.CELL_SIZE, Cell.CELL_SIZE, Cell.CELL_SIZE);
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int auxGetX(){
+		return x;
+	}
+	
+	public int auxGetY(){
+		return y;
 	}
 }

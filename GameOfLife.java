0961316b86fa931cc73489371f3d7883787e9621
andreimunteanu <mantundrei@gmail.com>
2013-03-10 14:53:34 +0100

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,81 +1,35 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Vector;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-
-
-public class GameOfLife extends JFrame {
+public class GameOfLife/* extends JFrame*/ {
 	private Grid grid;
-	private boolean running = false;
-	private boolean finish = true;
-	private static Integer workingPosition = 0;
+
+	private volatile Integer cleanerWorkingPos = 0;
+	private Integer terminatorWorkingPos=0;
+	private Integer generatorWorkingPos=0;
+	private volatile Integer runningThreads=4; 
+	private boolean initialization=true;
+
 	//private Cell[][] cells;
-	private volatile Vector<Cell> actualGeneration = new Vector<Cell>();
-	private volatile Vector<Cell> newGeneration = new Vector<Cell>();
-	private volatile Vector<Cell> toTerminateCells = new Vector<Cell>(); //da fare cambio di stato nel frame
-	private volatile Vector<Cell> possibleFutureGeneration = new Vector<Cell>(); //lista con cellule per la generazione futura non tutte ne fanno parte
-	// quelle che cambiano stato n b c'e da fare il cambio di stato nel frame
-	private JFrame main;
+	private Vector<Cell> actualGeneration = new Vector<Cell>();
+	private Vector<Cell> newGeneration = new Vector<Cell>();
+	private Vector<Cell> toTerminateCells = new Vector<Cell>(); //da fare cambio di stato nel frame
+	private Vector<Cell> possibleFutureGeneration = new Vector<Cell>(); 
+	// ho aggiunto la classe thread Synchronizer su cui viene fatta la join di fatto aspetta che ciascuna tipologia di thread finisca e crepa 
+	//così posso fare la wait sulle altre, Synchronizer riceve come parametro il workingPos di ciascun tipo di thread così può chiamare la notify 
+	// e svgliare le thread Corrispondenti
+	// ho aggiunto il metodo startThreads che riceve come parametro il workingPos e il vettore di thread corrispondente, serve solo a far partire 
+	// le thread
+	// il metodo runThread crea un nuovo siìync ci fa il la start join e questo fa notifyAll sul workinpos 
+	// bug nel decremento della variabile runningThreads che va a -8 dopo che le cleaner hanno lavorato
+	// è tutto un po' spartano perchè volevo vedere se anadava [:
 
 	public static void main(String[] args) {
 		new GameOfLife();
 	}
 
 	public GameOfLife(){
-		super("Game Of Life");
-		grid = new Grid(actualGeneration);
-		initFrame();
-		getContentPane().add(grid);
-		getContentPane().add(new startButton());
-		getContentPane().add(new pauseButton());
-		getContentPane().add(new clearButton());
-		initMenu();
-		setVisible(true);
 		setOff();
 	}
-
-	private void initFrame(){
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		getContentPane().setLayout(null);
-		setSize(grid.getXSize(), grid.getYSize() + (8 * Cell.CELL_SIZE));
-		setResizable(false);
-		setLocation(10,10);
-		//addCellsToFrame();
-		//initMenu();
-		//setVisible(true);
-	}
-
-
-	private void initMenu() {
-		JMenuBar menu = new JMenuBar();
-		menu.setOpaque(true);
-		menu.setBackground(Color.WHITE);
-		menu.setPreferredSize(new Dimension(0,20));
-		JMenu file = new JMenu("File");
-		JMenu size = new JMenu("Size");
-		JMenu edit = new JMenu("Edit");
-		JMenuItem exit = new JMenuItem("Exit");
-		JMenuItem size1 = new JMenuItem("50 x 50");
-		JMenuItem size2 = new JMenuItem("100 x 100");
-		JMenuItem size3 = new JMenuItem("200 x 200");
-		edit.add(size);
-		size.add(size1);
-		size.add(size2);
-		size.add(size3);
-		file.addSeparator();
-		file.add(exit);
-		menu.add(file);
-		menu.add(edit);
-		setJMenuBar(menu);
-	}
-
 	/*
 	private void test(){
 		for(int i=25;i < 28;i++){
@@ -85,93 +39,170 @@ public class GameOfLife extends JFrame {
 	}
 	 */
 	private void setOff(){
-		int coreN =1; //Runtime.getRuntime().availableProcessors();
+		int coreN =4;//Runtime.getRuntime().availableProcessors();
 		Cleaner[] cleaners = new Cleaner[coreN];
 		Terminator[] terminators = new Terminator[coreN];
 		Generator[] generators = new Generator[coreN];
+		System.out.println("Partito");
+		grid = new Grid(actualGeneration);
 		initThreads(cleaners,generators,terminators);
-		//grid = new Grid(actualGeneration);
 		//	grid.forceUpdate();
 		grid.test();
 		while(true){
 			try {
-				Thread.sleep(400);
+				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				System.err.println("Error in setOff() => " + e.getMessage());
 			}
-			if(running){
-				finish = false;
-				newGeneration(cleaners,generators,terminators);
-				//grid.removeCells(toTerminateCells);
-				//grid.addCells(newGeneration);
-				grid.forceUpdate(); //fa grid.repaint(); ogni 4 secondi (aggiustiamo poi);
-				//System.out.println("new" + newGeneration.size());
-				//System.out.println("to" + toTerminateCells.size());
-				//System.out.println("possible" + possibleFutureGeneration.size());
-				actualGeneration = newGeneration;
-				toTerminateCells = new Vector<Cell>();
-				possibleFutureGeneration = new Vector<Cell>();
-				//System.out.println(actualGeneration.size());
-				grid.forceUpdate();
-				newGeneration=new Vector<Cell>();
-				finish = true;
-			}
+			newGeneration(cleaners,generators,terminators);
+			//grid.removeCells(toTerminateCells);
+			//grid.addCells(newGeneration);
+			grid.forceUpdate(); //fa grid.repaint(); ogni 4 secondi (aggiustiamo poi);
+			//System.out.println("new" + newGeneration.size());
+			//System.out.println("to" + toTerminateCells.size());
+			//System.out.println("possible" + possibleFutureGeneration.size());
+			actualGeneration = newGeneration;
+			toTerminateCells=new Vector<Cell>();
+			possibleFutureGeneration=new Vector<Cell>();
+			//System.out.println(actualGeneration.size());
+			newGeneration=new Vector<Cell>();
+			cleanerWorkingPos=0;
+			terminatorWorkingPos=0;
+			generatorWorkingPos=0;
 		}
 	}
 
-	private void initThreads(Cleaner[] cleaners,Generator[] generators,Terminator[] terminators){
-		for(int i=0;i < cleaners.length;i++){
+	private void initThreads(Cleaner[] cleaners,Generator[] generators,Terminator[] terminators){ //metodo per inizializzare le thread 
+		for(int i=0;i < cleaners.length;i++){														
 			cleaners[i] = new Cleaner();
 			generators[i] = new Generator();
 			terminators[i] = new Terminator();
 		}
+		startThreads(cleaners,cleanerWorkingPos);
+
+		if(runningThreads !=0)
+			System.out.println("runningThreads=> " + runningThreads);
+		runningThreads = 4;
+		//System.out.println("Cleaner done");
+
+		startThreads(generators,generatorWorkingPos);
+
+		if(runningThreads !=0)
+			System.out.println("runningThreads=> " + runningThreads);
+		runningThreads = 4;
+
+		startThreads(terminators, terminatorWorkingPos);
+
+		if(runningThreads !=0)
+			System.out.println("runningThreads=> " + runningThreads);
+		runningThreads = 4;
+		initialization = false;
+
 	}
-	private void newGeneration(Cleaner[] cleaners,Generator[] generators,Terminator[] terminators){
+
+	private void startThreads(Thread[] slaves,Integer workingPos){
+		Synchronizer sync = new Synchronizer(workingPos); 	
+		sync.start();
+		for(int i=0; i < slaves.length;i++)
+			slaves[i].start();
+		try {
+			sync.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void newGeneration(Cleaner[] cleaners,Generator[] generators,Terminator[] terminators){ 
 		//System.out.println(workingPosition+" "+actualGeneration.size());
-		workingPosition = 0;
 		//System.out.println(cleaners[0].getState());
-		runThreads(cleaners);
-		//System.out.println("dopo la morte dei cleaners");
-		workingPosition = 0;
 
-		runThreads(terminators);
-		workingPosition = 0;
-		runThreads(generators);
+		System.out.println("runTs "+runningThreads);
+		runThreads(cleanerWorkingPos);
+		System.out.println("Ciao mam");
+		if(runningThreads !=0)
+			System.out.println("runningThreads=> " + runningThreads);
+
+		runningThreads = 4;
+		runThreads(generatorWorkingPos);
+
+		if(runningThreads !=0)
+			System.out.println("runningThreads=> " + runningThreads);
+
+		runningThreads = 4;
+		runThreads(terminatorWorkingPos);
+
+		if(runningThreads !=0)
+			System.out.println("runningThreads=> " + runningThreads);
+		runningThreads = 4;
 	}
 
-	private void runThreads(Thread[] slaves){
-		for(int i=0; i < slaves.length;i++)
-			slaves[i].run();
-
-		for(int i=0; i < slaves.length;i++)
-			try {
-				slaves[i].join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+	private void runThreads(Integer workingPos){
+		Synchronizer sync = new Synchronizer(workingPos); 
+		sync.start();
+		try {
+			sync.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	private class Synchronizer extends Thread{
+		private Integer workingPos;
+		public Synchronizer(Integer workingPos){
+			this.workingPos=workingPos;
+		}
+		public void run(){
+			synchronized (workingPos){
+				if(!initialization)
+					workingPos.notifyAll();
 			}
-
+			while(true){
+				System.out.println("syncher: thread attive" +runningThreads);
+				if(runningThreads == 0)
+					return;
+				else
+					try {
+						sleep(20);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+			}
+		}
 	}
+
 	private class Cleaner extends Thread{
 		private int i;
+		private boolean work=true;
 		public void run(){
 			while(true){
-				synchronized (workingPosition){
-					if(workingPosition < actualGeneration.size()){
-						//System.out.println("ciao non smetto" +actualGeneration.size());
-						i = workingPosition++;
+				synchronized (cleanerWorkingPos){
+					if(cleanerWorkingPos < actualGeneration.size()){
+						work=true;
+						System.out.println("Cleaner "+ this.getName() + (cleanerWorkingPos+1) + " size :"+actualGeneration.size());
+						i = cleanerWorkingPos++;
 
 					}
-					else{
-						//System.out.println("Morto un cleaner se ne fa un altro");
-						return;
-					}
+					else
+						try {
+							work=false;
+							runningThreads--;
+							System.out.println("runningThreads " + runningThreads);
+							cleanerWorkingPos.wait();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+
 				}
-				upDate(i);//potrebbe esserci race condition nelle variabile newGen e deadCells sicuramente
+				if(work)
+					upDate(i);//potrebbe esserci race condition nelle variabile newGen e deadCells sicuramente
 			}
 		}
 
 		private void upDate(int index){//race condition
-			if(actualGeneration.size() > 0){
+			if(actualGeneration.size() > 0 && index < actualGeneration.size()){
 				int aliveNeighbors = watchNeighbors(actualGeneration.get(index));
 				if(aliveNeighbors == 2 || aliveNeighbors == 3)
 					newGeneration.add(actualGeneration.get(index));
@@ -211,17 +242,27 @@ public class GameOfLife extends JFrame {
 
 	private class Terminator extends Thread{
 		private int i;
+		private boolean work;
 		public void run(){
 			//System.out.println("Terminator online");
 			while(true){
-				synchronized (workingPosition){
-					if(workingPosition < toTerminateCells.size()){
-						i = workingPosition++;
-					}
-					else
-						return;
+				synchronized (terminatorWorkingPos){
+					if(terminatorWorkingPos < toTerminateCells.size()){
+						work=true;
+						i = terminatorWorkingPos++;
+					} else{
+						work=false;
+						runningThreads--;
+						try {
+							terminatorWorkingPos.wait();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}	
 				}
-				killCell(i);
+				if(work)
+					killCell(i);
 			}
 		}
 		private void killCell(int index) {
@@ -235,80 +276,38 @@ public class GameOfLife extends JFrame {
 
 	private class Generator extends Thread{
 		private int i;
+		private boolean work;
 		public void run(){
 			//System.out.println("Generator online");
 			while(true){
-				synchronized(workingPosition){
-					if(workingPosition < possibleFutureGeneration.size()){
-						i = workingPosition++;
-					}
-					else
-						return;
+				synchronized(generatorWorkingPos){
+					if(generatorWorkingPos < possibleFutureGeneration.size()){
+						work=true;
+						i = generatorWorkingPos++;
+					} else
+						try {
+							work=false;
+							runningThreads--;
+							generatorWorkingPos.wait();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
 				}
-				checkDeadCell(i);
+				if(work)
+					checkDeadCell(i);
 			}
 		}
 
 		private void checkDeadCell(int index){
-			Cell cell = possibleFutureGeneration.get(index);
-			if(grid.getNumbOfN(cell) == 3){
-				newGeneration.add(grid.createLivingCell(cell));
+			if(possibleFutureGeneration.size() > 0){
+				Cell cell = possibleFutureGeneration.get(index);
+				if(grid.getNumbOfN(cell) == 3){
+					newGeneration.add(grid.createLivingCell(cell));
+				}
+				else
+					grid.resetCell(cell);
 			}
-			else
-				grid.resetCell(cell);
-		}
-	}
-
-	private class startButton extends JButton{
-		protected startButton(){
-			super("START");
-			setBounds(0, grid.getYSize(), grid.getXSize() / 3, 3 * Cell.CELL_SIZE	);
-			this.addActionListener(new ActionListener(){
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					running = true;
-				}
-			});
-		}
-	}
-
-	private class pauseButton extends JButton{
-		protected pauseButton(){
-			super("PAUSE");
-			setBounds(grid.getXSize() / 3, grid.getYSize(), grid.getXSize() / 3, 3 * Cell.CELL_SIZE	);
-			this.addActionListener(new ActionListener(){
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					running = false;
-				}
-			});
-		}
-	}
-
-	private class clearButton extends JButton{
-		protected clearButton(){
-			super("CLEAR");
-			setBounds(2 * (grid.getXSize() / 3), grid.getYSize(), grid.getXSize() / 3, 3 * Cell.CELL_SIZE	);
-			this.addActionListener(new ActionListener(){
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					boolean temp = running;
-					running = false;
-					while(!finish); //BUSY WAITING :D :D :) :| :( D: anche se poco
-					//e non risolve comunque il bug (prova a fare clear dopo la pausa)
-					//c'è pieno di bug in realtà, forse la logica è da rivedere completamente
-					//per esempio, se facciamo una figura che poi scompare completamente, poi premi su "CLEAR"
-					//e dice che actual generation ha 9 elementi invece di 0
-					possibleFutureGeneration = new Vector<Cell>();
-					newGeneration = new Vector<Cell>();
-					toTerminateCells = new Vector<Cell>();
-					grid.clearGrid();
-					actualGeneration = new Vector<Cell>();
-					running = temp;
-					grid.forceUpdate();
-					getContentPane().repaint();
-				}
-			});
 		}
 	}
 }

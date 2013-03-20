98e55058d7a695c5927274540d22655982033fe8
niccolomarastoni <eightnine@gmail.com>
2013-03-20 01:19:57 +0100

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -5,6 +6,8 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 
+//Adesso che abbiamo un tipo solo di cellula è ora di mandare in pensione questo file
+//così la smettiamo con tutti quei cast inutili in Grid
 public abstract class Cell extends JButton{
 	private int x;
 	private int y;
@@ -15,23 +18,27 @@ public abstract class Cell extends JButton{
 		setBounds(x * Cell.CELL_SIZE, y * Cell.CELL_SIZE, Cell.CELL_SIZE, Cell.CELL_SIZE);
 		this.x = x;
 		this.y = y;
-		
+
 	}
-	
+
 	public int auxGetX(){
 		return x;
 	}
-	
+
 	public int auxGetY(){
 		return y;
 	}
-	
-	public abstract boolean isLivingCell();
-	
-	
+
+	public abstract boolean isAliveNow();
+	public abstract boolean isAliveNext();
+	public abstract void swap();
+
 	@Override
 	public String toString(){
 		return "(" + auxGetX() + ", " + auxGetY() + ")";
 	}
+
 	
+
+
 }

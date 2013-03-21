49409fc2d47845274eb1fe1041b685d686ed0fb2
andreimunteanu <mantundrei@gmail.com>
2013-03-21 17:47:07 +0100

    Update Figures.java

--- Figures.java
+++ Figures.java
@@ -1,11 +1,19 @@
 public final class Figures {
-  private static int[][] blinker = {{0,-1},{0,0},{0,1} };
-	
+	private static final int[][] blinker = {{0,-1},{0,0},{0,1} };
+	private static final int[][] pulsar = {{1,-2},{1,-3},{1,-4},{2,-6},{3,-6},{4,-6},{2,-1},{3,-1},{4,-1},{6,-2},{6,-3},{6,-4},
+						{2,1},{3,1},{4,1},{1,2},{1,3},{1,4},{2,6},{3,6},{4,6},{6,2},{6,3},{6,4},
+						{-2,-1},{-3,-1},{-4,-1},{-1,-2},{-1,-3},{-1,-4},{-2,-6},{-3,-6},{-4,-6},{-6,-2},{-6,-3},{-6,-4},
+						{-2,1},{-3,1},{-4,1},{-1,2},{-1,3},{-1,4},{-2,6},{-3,6},{-4,6},{-6,2},{-6,3},{-6,4}};
+	private static final int[][] glider = {{0,0},{0,-1},{0,-2},{-1,0},{-2,-1}};
+	private static final int[][] lwss = {{0,0},{1,0},{2,0},{3,0},{4,-1},{0,-1},{0,-2},{1,-3},{4,-3}};			
 	
 	public static int[][] getCoordinates(String figureName){
 		int[][] temp = null;
 		switch(figureName){
 		case "blinker": temp = blinker;break;
+		case "pulsar": temp = pulsar;break;
+		case "glider": temp = glider;break;
+		case "lwss": temp = lwss;break;
 		default:break;
 		}
 			

4c5c1e74ed22e51cd802c0eaa374d77a9558b2c8
niccolomarastoni <eightnine@gmail.com>
2013-03-22 16:48:57 +0100

    fixed java 6 compatibility issue

--- Figures.java
+++ Figures.java
@@ -1,13 +1,23 @@
 public final class Figures {
 	private static final int[][] blinker = {{0,-1},{0,0},{0,1} };
 	private static final int[][] pulsar = {{1,-2},{1,-3},{1,-4},{2,-6},{3,-6},{4,-6},{2,-1},{3,-1},{4,-1},{6,-2},{6,-3},{6,-4},
-						{2,1},{3,1},{4,1},{1,2},{1,3},{1,4},{2,6},{3,6},{4,6},{6,2},{6,3},{6,4},
-						{-2,-1},{-3,-1},{-4,-1},{-1,-2},{-1,-3},{-1,-4},{-2,-6},{-3,-6},{-4,-6},{-6,-2},{-6,-3},{-6,-4},
-						{-2,1},{-3,1},{-4,1},{-1,2},{-1,3},{-1,4},{-2,6},{-3,6},{-4,6},{-6,2},{-6,3},{-6,4}};
+		{2,1},{3,1},{4,1},{1,2},{1,3},{1,4},{2,6},{3,6},{4,6},{6,2},{6,3},{6,4},
+		{-2,-1},{-3,-1},{-4,-1},{-1,-2},{-1,-3},{-1,-4},{-2,-6},{-3,-6},{-4,-6},{-6,-2},{-6,-3},{-6,-4},
+		{-2,1},{-3,1},{-4,1},{-1,2},{-1,3},{-1,4},{-2,6},{-3,6},{-4,6},{-6,2},{-6,3},{-6,4}};
 	private static final int[][] glider = {{0,0},{0,-1},{0,-2},{-1,0},{-2,-1}};
 	private static final int[][] lwss = {{0,0},{1,0},{2,0},{3,0},{4,-1},{0,-1},{0,-2},{1,-3},{4,-3}};			
-	
+
 	public static int[][] getCoordinates(String figureName){
+		if(figureName.equals("blinker"))
+			return blinker;
+		else if(figureName.equals("pulsar"))
+			return pulsar;
+		else if(figureName.equals("glider"))
+			return glider;
+		
+		return lwss;
+	}
+		/*
 		int[][] temp = null;
 		switch(figureName){
 		case "blinker": temp = blinker;break;
@@ -16,7 +26,8 @@ public final class Figures {
 		case "lwss": temp = lwss;break;
 		default:break;
 		}
-			
-			return temp;
-	}
+
+		return temp;
+		*/
+	
 }

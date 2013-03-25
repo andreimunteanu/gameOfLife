0754af40539e80246259290e284e9fbbfb0abdef
andreimunteanu <mantundrei@gmail.com>
2013-03-25 22:50:32 +0100

    Update Figures.java

--- Figures.java
+++ Figures.java
@@ -1,9 +1,24 @@
+
+/**
+ * 
+ * @
+ *
+ */
 public final class Figures {
+	
+	/**
+	 * 
+	 */
 	private static final int[][] blinker = {{0,-1},{0,0},{0,1} };
 	private static final int[][] pulsar = {{0,-2},{0,2},{2,0},{2,1},{2,-1},{2,2},{2,-2},{-2,0},{-2,1},{-2,-1},{-2,2},{-2,-2}};
 	private static final int[][] glider = {{0,0},{0,-1},{0,-2},{-1,0},{-2,-1}};
 	private static final int[][] lwss = {{0,0},{1,0},{2,0},{3,0},{4,-1},{0,-1},{0,-2},{1,-3},{4,-3}};			
 
+	/**
+	 * 
+	 * @param figureName
+	 * @return
+	 */
 	public static int[][] getCoordinates(String figureName){
 		if(figureName.equals("blinker"))
 			return blinker;

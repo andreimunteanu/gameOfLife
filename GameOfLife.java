13a790b5c7c4db2a4dab50a4ad84ca0212a654da
andreimunteanu <mantundrei@gmail.com>
2013-03-26 22:24:07 +0100

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -3,9 +3,7 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Hashtable;
-import java.util.Vector;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
